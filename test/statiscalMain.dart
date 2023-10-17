5a26a08d20b587852684e42cb831ed0c833dfa81
NTGiang2k2 <0306201019@caothang.edu.vn>
2023-10-17 13:53:47 +0700

    statiscalMain v1

---
+++ test/statiscalMain.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StatiscalMain extends StatelessWidget{
+
+  int soDonHangThanhCong=0;
+  int soDonHangHuy=0;
+  int soDonHangCho=0;
+  int tongSoTienChiTieu=0;
+
+  StatiscalMain({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        const Center(
+          child: Text('Thống kê',
+            style: TextStyle(
+              fontSize: 50,
+            ),
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Số lương đơn hàng thành công:$soDonHangThanhCong'),
+            Text('Số lượng đơn hàng hủy:$soDonHangHuy'),
+            Text('Số lượng đơn hàng chờ:$soDonHangCho'),
+            Text('Tổng số tiền chi tiêu:$tongSoTienChiTieu'),
+          ],
+        ),
+      ],
+    );
+  }
+}
